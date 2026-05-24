d26a573b916c7ccd9c6669994a7f944a561ad126
MrMongy01 <luiscarlosmoherhdz@gmail.com>
2026-05-23 21:28:33 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+function calcular(){
+
+    let promedio = parseFloat(document.getElementById("promedio").value);
+
+    let colegiatura = parseFloat(document.getElementById("colegiatura").value);
+
+    let meses = parseInt(document.getElementById("meses").value);
+
+    let resultado = document.getElementById("resultado");
+
+    let beca = 0;
+
+    // CALCULAR BECA
+
+    if(promedio >= 95){
+        beca = 1.0;
+    }
+    else if(promedio >= 90){
+        beca = 0.80;
+    }
+    else if(promedio >= 86){
+        beca = 0.60;
+    }
+
+    //BECAAPROBADA
+
+    if(beca > 0){
+
+        let descuento = colegiatura * beca;
+
+        let total = colegiatura - descuento;
+
+        resultado.innerHTML = `
+            <h2>Beca otorgada</h2>
+
+            <p><strong>Porcentaje de beca:</strong> ${beca * 100}%</p>
+
+            <p><strong>Descuento:</strong> $${descuento.toFixed(2)}</p>
+
+            <p><strong>Total a pagar:</strong> $${total.toFixed(2)}</p>
+        `;
+    }
+
+    //NOBECA
+
+    else{
+
+        let interes = colegiatura * 0.10;
+
+        let totalConInteres = colegiatura + interes;
+
+        let pagoMensual = totalConInteres / meses;
+
+        resultado.innerHTML = `
+            <h2>No aplica para beca</h2>
+
+            <p><strong>Total sin interés:</strong> $${colegiatura.toFixed(2)}</p>
+
+            <p><strong>Total con interés:</strong> $${totalConInteres.toFixed(2)}</p>
+
+            <p><strong>Pago mensual:</strong> $${pagoMensual.toFixed(2)}</p>
+        `;
+    }
+}
